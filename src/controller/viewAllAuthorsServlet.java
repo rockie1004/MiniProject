3d5dbcda00c99ba7bc4db35ed680d6537ffcc35d
Amy Rockie <rockie1004@gmail.com>
2020-02-29 18:44:44 -0600

    Added author servlets

---
+++ src/controller/viewAllAuthorsServlet.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class viewAllAuthorsServlet
+ */
+@WebServlet("/viewAllAuthorsServlet")
+public class viewAllAuthorsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public viewAllAuthorsServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ListAuthorHelper dao = new ListAuthorHelper();
+		request.setAttribute("allAuthors", dao.showAllAuthors());
+		
+		String path = "/author-list.jsp";
+		
+		if(dao.showAllAuthors().isEmpty()) {path = "/index.html";}
+	
+		getServletContext().getRequestDispatcher(path).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
